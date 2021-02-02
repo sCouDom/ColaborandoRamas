f3b7e41cd7a863e1ae273706461dfe9d495b4714
Sergio Counago Dominguez <scounagodominguez@danielcastelao.org>
2021-02-02 11:02:37 +0100

    closes #1, closes #2 método corregido

--- src/com/company/MetodoSergio.java
+++ src/com/company/MetodoSergio.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class MetodoSergio {
     public static void nombreSergio(){
-        System.out.println("Rama Sergio");
+        System.out.println("Rama Sergio Corregido");
     }
 }
